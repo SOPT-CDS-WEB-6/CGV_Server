8c5966f621a03b30b52121c91294b6dffda9c810
Lee ChanMi <anytime0224@gmail.com>
2023-06-24 14:52:47 +0900

    [Refactor] update md

--- src/main/java/com/cds/cgv/common/status/ErrorStatus.java
+++ src/main/java/com/cds/cgv/common/status/ErrorStatus.java
@@ -19,6 +19,11 @@ public enum ErrorStatus {
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "fileType 값을 확인하세요."),
     INVALID_INQUIRY_TYPE(HttpStatus.BAD_REQUEST, "문의 유형(type) 값을 확인하세요."),
 
+    NOT_FOUND_SAVE_IMAGE_EXCEPTION(HttpStatus.BAD_REQUEST, "이미지 저장에 실패했습니다"),
+    NOT_FOUND_FILE_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 파일입니다"),
+    NOT_FOUND_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "이미지 이름을 찾을 수 없습니다"),
+    INVALID_MULTIPART_EXTENSION_EXCEPTION(HttpStatus.BAD_REQUEST, "허용되지 않은 타입의 파일입니다")
+
     ;
 
     private final HttpStatus httpStatus;
